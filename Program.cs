cedb61a616506c37ffba62399cfe37047be61092
RavulaSonali <ravula.sonali@gmail.com>
2023-11-09 14:34:02 +0530

    Assignment 4-part1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Assignment_4_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name="Ravula Sonali";
+            int age=23;
+            double heightInCentimeters = 180.5;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + heightInCentimeters);
+            Console.ReadKey();
+        }
+    }
+}
